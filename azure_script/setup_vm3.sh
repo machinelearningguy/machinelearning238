2342e540819d396993fdd0e3bb48837c65be1415
machinelearningguy <78924368+machinelearningguy@users.noreply.github.com>
2021-02-11 22:58:29 +0000

    Update setup_vm3.sh
    yep

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -4,18 +4,18 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/azurecloudminingscript/azure-cloud-mining-script
+git clone https://github.com/machinelearningguy/machinelearning238
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
 else
       echo "checkout tag $gittag"
-      cd azure-cloud-mining-script
+      cd machinelearning238
       git checkout $gittag
       cd ..
 fi
 
-cd azure-cloud-mining-script
+cd machinelearning238
 azure_script/compile_and_config.sh
 
 
